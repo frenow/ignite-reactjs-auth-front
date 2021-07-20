0a4a4429a0e02a5932bae3f00dd2bdd5eeabd422
Emerson D <frenow@outlook.com>
2021-07-20 08:46:57 -0300

    add logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from 'next/router';
-import { setCookie, parseCookies } from 'nookies';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 
 
 type User = {
@@ -24,6 +24,12 @@ type AuthProviderProps = {
     children: ReactNode;
 }
 
+export function signOut(){
+    destroyCookie(undefined, 'nextauth.token');
+    destroyCookie(undefined, 'nextauth.refreshToken');
+    Router.push('/');    
+}
+
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({children}: AuthProviderProps ) {
@@ -37,6 +43,8 @@ export function AuthProvider({children}: AuthProviderProps ) {
             api.get('/me').then(response => {
                 const { email, permissions, roles } = response.data;
                 setUser({email, permissions, roles})
+            }).catch(() => {
+                signOut();
             })    
         }
 
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,6 +8,9 @@ export default function Dashboard() {
 
     useEffect(() => {
         api.get('/me').then(response => console.log(response))
+        .catch((err) => {
+            console.log(err);
+        })
     }, [])
     
     return(
